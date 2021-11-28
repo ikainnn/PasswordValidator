ee2dd7b1196d3a7b234433d42a6338106c3aca3d
ikainnn <36525792+ikainnn@users.noreply.github.com>
2021-11-28 16:24:53 -0300

    Update charset.hpp

--- pswd/charset/charset.hpp
+++ pswd/charset/charset.hpp
@@ -3,8 +3,11 @@
 
 namespace pswd::charset
 {
+    constexpr std::size_t g_charsetPoolSize = 68;
+
     constexpr std::array<char, 10> g_numericCharset {
-        '0', '1', '2',  '3', '4', '5',  '6', '7', '8', '9' };
+        '0', '1', '2',  '3', '4', 
+        '5',  '6', '7', '8', '9' };
 
     constexpr std::array<char, 26> g_upperCharset   {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
