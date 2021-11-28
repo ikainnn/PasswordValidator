7b2528ee5b83c41331357b4e78a10de5e9724a69
ikainnn <36525792+ikainnn@users.noreply.github.com>
2021-11-28 16:17:46 -0300

    Add files via upload

--- pswd/security.cpp
+++ pswd/security.cpp
@@ -2,26 +2,46 @@
 
 namespace pswd
 {    
-    static std::double_t error_message(const std::string_view& error) 
+    static std::int32_t error_message(const std::string_view& error) 
     {
         std::cout << error << '\n'; return 0.0;
     }
 
     static std::double_t calc_password_entropy(const std::size_t passwordSize) 
     {
+        auto charsetSize = pswd::charset::g_upperCharset.size() +
+                           pswd::charset::g_numericCharset.size() +
+                           pswd::charset::g_specialCharset.size();
+
         // source: https://generatepasswords.org/how-to-calculate-entropy/
-        return std::log2(std::pow(pswd::charset::g_charsetPool.size(), passwordSize));
+        return std::log2(std::pow(charsetSize, passwordSize));
     }
     
-    static bool search_charset_chars(const std::string_view& password)
+    template <std::size_t SZ>
+    static bool search_charset_chars(const std::string_view& password, const std::array<char, SZ>& charset)
     {
         auto result = std::find_if(
-            password.begin(), password.end(), [](const auto& currentChar) {
-                auto result = std::find(pswd::charset::g_charsetPool.begin(),
-                                        pswd::charset::g_charsetPool.end(), currentChar);
-                return result != pswd::charset::g_charsetPool.end();
+            password.begin(), password.end(), [&charset](const auto& currentChar) {       
+                return std::find(charset.begin(), charset.end(), currentChar) != charset.end();
             });
         return result != password.end();
+    }
+
+    static bool search_charset_chars(const std::string_view& password)
+    {
+        // checks if the password contains uppercase chars
+        if (!search_charset_chars(password, pswd::charset::g_upperCharset))
+            return error_message("your password must contain atleast one uppercase character.");
+
+        // checks if the password contains numeric chars
+        if (!search_charset_chars(password, pswd::charset::g_numericCharset))
+            return error_message("your password must contain atleast one one digit.");
+
+        // checks if the password contains special chars
+        if (!search_charset_chars(password, pswd::charset::g_specialCharset))
+            return error_message("your password must contain atleast one symbol.");
+        
+        return true;
     }
     
     static std::int32_t search_repeated_chars(const std::string_view& password)
@@ -49,7 +69,7 @@ namespace pswd
             });
         return result;
     }
-    
+
     std::string_view parse_entropy(std::double_t entropy)
     {
         if      (entropy <= 0.0f)
@@ -74,8 +94,8 @@ namespace pswd
             return error_message("invalid password size.");
         
         // checks if the password contains characters from the charset.  ie: 0, A, [...
-        if(!search_charset_chars(password))
-            return error_message("your password must contain: one symbol, one digit and one uppercase character.");
+        if (!search_charset_chars(password))
+            return 0;
         
         // checks if the password has more than 2 repeated characters.   ie: FFF...
         if (search_repeated_chars(password) > 2)
