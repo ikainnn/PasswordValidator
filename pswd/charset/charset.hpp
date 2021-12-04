2f86782d01d6bfbd196dca3c940deb9209f0ba8f
ikainnn <36525792+ikainnn@users.noreply.github.com>
2021-12-04 19:18:47 -0300

    Add files via upload

--- pswd/charset/charset.hpp
+++ pswd/charset/charset.hpp
@@ -3,20 +3,10 @@
 
 namespace pswd::charset
 {
-    constexpr std::size_t g_charsetPoolSize = 68;
-
-    constexpr std::array<char, 10> g_numericCharset {
-        '0', '1', '2',  '3', '4', 
-        '5',  '6', '7', '8', '9' };
-
-    constexpr std::array<char, 26> g_upperCharset   {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-    constexpr std::array<char, 32> g_specialCharset {
-        '!', '"',  '#', '$', '%', '&', '\'', '(', ')', '*', '+',
-        ',', '-',  '.', '/', ':', ';', '<',  '=', '>', '?', '@',
-        '[', '\\', ']', '^', '_', '`', '{',  '|', '}', '~' };
+    constexpr std::array<char, 32> g_specialCharset{
+        '"', '#',  '$', '%', '&', '\'', '(', ')', '*', '+', ',',
+        '-', '.',  '/', ':', ';', '<',  '=', '>', '?', '!', '@',
+        '[', '\\', ']', '^', '_', '`',  '{', '|', '}', '~'};
 }
 
 #endif /* A79329D4_667B_4891_BB3B_052F4C7CC173 */
