5a036204dff6a671234387d49e002cc2ab6be688
ikainnn <nyakonyns@gmail.com>
2022-03-11 12:03:43 -0300

    Fixed broken documentation comment

--- source/entropy/validation/PasswordValidation.hpp
+++ source/entropy/validation/PasswordValidation.hpp
@@ -5,6 +5,10 @@
 
 namespace pswd::entropy::validation
 {
+    bool validate_password_length(std::string_view password);
+    bool validate_password_ordering(std::string_view password);
+    bool validate_password_symbols(std::string_view password);
+
     /**
      * The function checks if the password contains atleast 2 digits, 2 uppercase letters and 2 special
      * symbols
@@ -13,11 +17,6 @@ namespace pswd::entropy::validation
      * 
      * @return True if the password is a valid password, or false if it isnt.
      */
-    
-    bool validate_password_length(std::string_view password);
-    bool validate_password_ordering(std::string_view password);
-    bool validate_password_symbols(std::string_view password);
-
     template <class T> inline bool validate_password(T&& password)
     {
         return validate_password_length(std::forward<T>(password)) && 
