5bc0c159d54391ad4bcbf8cd0c24000f9c227da5
ikainnn <nyakonyns@gmail.com>
2022-03-11 11:45:51 -0300

    Added newline character to messageFormat.

--- source/PasswordValidator.cpp
+++ source/PasswordValidator.cpp
@@ -9,7 +9,7 @@ std::string get_user_password();
 
 std::int32_t main()
 {
-    constexpr static std::string_view c_messageFormat = "Your password entropy has a value of {}, which is considered {}.";
+    constexpr static std::string_view c_messageFormat = "Your password entropy has a value of {}, which is considered {}.\n";
 
     const auto password = get_user_password();
     const auto passwordEntropy = pswd::entropy::calculate_password_entropy(password);
